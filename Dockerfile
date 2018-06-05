85fe8d7a76048604770c92da25ca30795d4c444d
Gökmen Görgen <gkmngrgn@gmail.com>
2018-06-05 02:42:42 +0200

    initial commit.

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM python:3.6-alpine
+WORKDIR /code
+ADD . /code
+RUN pip install -r requirements.txt
+ENTRYPOINT "/code/entrypoint.sh"
+CMD ["python", "manage.py", "runserver", "0:8000"]
+EXPOSE 8000
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+import admin_readonly_fields
+
+description = 'Show readonly fields better in Django admin panel'
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='django-admin-readonly-fields',
+    version=admin_readonly_fields.__version__,
+    description=description,
+    author='Gökmen Görgen',
+    author_email='gkmngrgn@gmail.com',
+    url='https://github.com/gkmngrgn/django-admin-readonly-fields',
+    long_description=long_description,
+    packages=[
+        'admin_readonly_fields',
+    ],
+    package_data={
+        'admin_readonly_fields': [
+            'static/admin_readonly_fields/*.js',
+            'static/admin_readonly_fields/*.css',
+            'static/admin_readonly_fields/img/*',
+            'static/django-admin_readonly_fields/*.js',
+        ]
+    },
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[]
+)
